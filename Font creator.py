3905c97d249d6db93398dc37cbf39d9d85c177cf
Devadath.P.R <prdevadathk3@gmail.com>
2023-05-12 00:56:26 +0530

    Updated FontCreator.py

--- Font creator.py
+++ Font creator.py
@@ -16,17 +16,26 @@ class main:
         self.first_coordinate = True  # flag to track whether this is the first coordinate in the stroke
         self.master.bind('<KeyPress>', self.on_key_press)
         self.current_coordinate = 0  # counter to track the current coordinate in the matrix
+        self.y_ofst = 230
+        self.versions = 4
+        self.prompts = []
+        self.prompt_index = 0
+        self.prompt_generator(4)
+        self.NewText = ("Draw " + self.prompts[self.prompt_index])
+        self.changeHeaderText(self.NewText)
+
+
 
     def paint(self, e):
         if self.old_x and self.old_y:
             self.c.create_line(self.old_x, self.old_y, e.x, e.y, width=self.penwidth, fill=self.color_fg, capstyle=ROUND, smooth=True)
 
         # update the coordinates matrix with the current coordinates
-        self.coordinates_matrix.append([e.x, self.c.winfo_height() - e.y])
+        self.coordinates_matrix.append([e.x, self.y_ofst - e.y])
 
         self.old_x = e.x
         self.old_y = e.y
-        self.coordinates['text'] = f'X: {e.x} Y: {self.c.winfo_height() - e.y}'
+        self.coordinates['text'] = f'X: {e.x} Y: {self.y_ofst - e.y}'
 
     def reset(self, e):
         # add 69420 for both X and Y when the pen stroke ends
@@ -39,27 +48,45 @@ class main:
     def on_key_press(self, e):
         if e.char == ' ':
             self.normalize_coordinates()
-            print(self.coordinates_matrix)
+            #print(self.coordinates_matrix)
             self.save_coordinates()
+            self.clear()
+            if (len(self.prompts) > self.prompt_index + 1):
+                self.prompt_index += 1 
+                self.NewText = ("Draw " + self.prompts[self.prompt_index])
+                self.changeHeaderText(self.NewText)
+            else: 
+                self.NewText = "Adios"
+                self.changeHeaderText(self.NewText)
+        if e.char == '\b':
+            self.clear()
     
     def normalize_coordinates(self):
         # find the minimum X and Y values
         min_x = min(coord[0] for coord in self.coordinates_matrix)
-        min_y = min(coord[1] for coord in self.coordinates_matrix)
+        #min_y = min(coord[1] for coord in self.coordinates_matrix)
 
         # subtract the minimum X and Y values from all values in the matrix
         for coord in self.coordinates_matrix:
             coord[0] -= min_x
-            coord[1] -= min_y
+            #coord[1] -= min_y
+
+    def prompt_generator(self,n):
+        for i in range (33,123):
+            for j in range(1,n):
+                self.prompts.append(chr(i) + str(j))
+        #print(*self.prompts, sep = "\n")
+                
 
     def save_coordinates(self):
         # open the file in write mode
-        with open("TestFont.txt", "w") as f:
+        with open("Font.txt", "a") as f:
+            f.write(self.prompts[self.prompt_index]+"<\n")
             # write "U" before the first coordinate
             f.write("U\n")
             for coord in self.coordinates_matrix:
                 if self.first_coordinate:
-                    f.write(f"{coord[0]} {coord[1]}\n")
+                    f.write("G1 X" + f"{coord[0]/10}" + " Y" + f"{coord[1]/10}\n")
                     self.current_coordinate += 1
                     f.write("D\n")
                     self.first_coordinate = False
@@ -67,8 +94,9 @@ class main:
                     f.write("U\n")
                     self.first_coordinate = True
                 else:
-                    f.write(f"{coord[0]} {coord[1]}\n")
+                    f.write("G1 X" + f"{coord[0]/10}" + " Y" + f"{coord[1]/10}\n")
                 self.current_coordinate += 1
+            f.write(">\n")
 
 
     def changeW(self,e): #change Width of pen through slider
@@ -76,22 +104,27 @@ class main:
            
 
     def clear(self):
+        self.coordinates_matrix = []
         self.c.delete(ALL)
+        self.c.create_line(0,150,3000,150)
+        self.c.create_line(0,230,3000,230)
+    
+    def changeHeaderText(self, NewText):
+        self.header.config(text=NewText)
+
 
     def drawWidgets(self):
-        self.controls = Frame(self.master,padx = 5,pady = 5)
-        self.clear_btn = Button(self.controls, text='Clear Canvas', command=self.clear)
-        self.clear_btn.grid(row=0,column=0)
+        self.controls = Frame(self.master,padx = 10,pady = 10)
+        self.header = Label(self.controls, text='Get ready',font=('arial 20'))
+        self.header.grid(row=0,column=0, sticky=W)
         self.coordinates = Label(self.controls, text='')
-        self.coordinates.grid(row=1,column=0)
-        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=2,column=0)
-        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=VERTICAL)
-        self.slider.set(self.penwidth)
-        self.slider.grid(row=2,column=1,ipadx=30)
+        self.coordinates.grid(row=2,column=0)
         self.controls.pack(side=LEFT)
-        
         self.c = Canvas(self.master,width=500,height=400,bg=self.color_bg,)
         self.c.pack(fill=BOTH,expand=True)
+        self.c.create_line(0,150,3000,150)
+        self.c.create_line(0,230,3000,230) 
+
 
 
 
